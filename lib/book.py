28270c65e8ef645b264ed8debe408dd74b992060
ckorir <chrisruto.k@gmail.com>
2023-11-30 12:21:45 +0300

    Completed assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
 class Book:
+    def __init__(self, title, page_count):
+        self.title = title
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+    
+    def get_page_count(self):
+        return self._page_count
+    
+    def set_page_count(self, new_page_count):
+        if isinstance(new_page_count, int):
+            self._page_count = new_page_count
+        else:
+            print("page_count must be an integer")
+
+    page_count = property(get_page_count, set_page_count)
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
     pass
     
         
